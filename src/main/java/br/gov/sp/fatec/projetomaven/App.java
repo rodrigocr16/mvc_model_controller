dbb5bf6a9a444accd0d95fa8c8ba3f2a900a1818
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2020-09-11 20:38:04 +0000

    Aula_JPA1

--- src/main/java/br/gov/sp/fatec/projetomaven/App.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/App.java
@@ -1,5 +1,10 @@
 package br.gov.sp.fatec.projetomaven;
 
+import javax.persistence.Persistence;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import br.gov.sp.fatec.projetomaven.entity.Aluno;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("avaliacao"); // Nome da persistency unit
+        EntityManager manager = factory.createEntityManager();
+        
+        Aluno aluno = new Aluno();
+        aluno.setNomeUsuario("aluno");
+        aluno.setSenha("senha");
+        aluno.setRa(1234567891011L);
+
+        try{
+            manager.getTransaction().begin();
+            manager.persist(aluno);
+            manager.getTransaction().commit();
+        }
+        catch(Exception e){
+            manager.getTransaction().rollback();
+        }
+        System.out.println(aluno.getId());
+        manager.close();
     }
 }
