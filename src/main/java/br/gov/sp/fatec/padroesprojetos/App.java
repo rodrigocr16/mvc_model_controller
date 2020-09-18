ce426c37081f5ef8b90d775baa51619f63d2a19e
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2020-09-18 14:29:52 +0000

    Primeiro CRUD!

--- src/main/java/br/gov/sp/fatec/padroesprojetos/App.java
+++ src/main/java/br/gov/sp/fatec/padroesprojetos/App.java
@@ -1,6 +1,8 @@
 package br.gov.sp.fatec.padroesprojetos;
 
 import br.gov.sp.fatec.padroesprojetos.dao.UsuarioDaoJpa;
+import br.gov.sp.fatec.padroesprojetos.entity.Usuario;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -14,11 +16,22 @@ public class App
             .createEntityManagerFactory("padroesprojetos");
         EntityManager manager = factory.createEntityManager();
         
-
+        // CREATE
         UsuarioDaoJpa usuarioDaoJpa = new UsuarioDaoJpa(manager);
-        usuarioDaoJpa.cadastrarUsuario("MelhorPinto", "DonaPinta123", "Robertinho Sensato");
+        usuarioDaoJpa.cadastrarUsuario("usuario01", "senha", "jhon doe");
+        usuarioDaoJpa.cadastrarUsuario("usuario02", "senha", "jane doe");
+        usuarioDaoJpa.cadastrarUsuario("usuario03", "senha", "jack doe");
 
-                
+        // READ
+        System.out.println(usuarioDaoJpa.buscarUsuario("usuario01").getNomeExibicao());
         
+        // UPDATE
+        Usuario usuario = new Usuario();
+        usuario = usuarioDaoJpa.buscarUsuario("usuario03");
+        usuario.setSenha("senhaF0rt£");
+        usuarioDaoJpa.commitUsuario(usuario);
+
+        // DELETE
+        usuarioDaoJpa.removerUsuario("usuario02");
     }
 }
