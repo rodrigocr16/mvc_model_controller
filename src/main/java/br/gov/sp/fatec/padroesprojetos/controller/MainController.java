18e4e3fbc38592980de58ebbaa4e07a138be07f6
rodrigocr16 <rodrigo.reis13@fatec.sp.gov.br>
2020-10-22 00:40:45 +0000

    aula_web_04

---
+++ src/main/java/br/gov/sp/fatec/padroesprojetos/controller/MainController.java
@@ -0,0 +1,18 @@
+package br.gov.sp.fatec.padroesprojetos.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainController extends HttpServlet{
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Bem vindo!");
+    }
+}
